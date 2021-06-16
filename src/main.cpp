1c866ce63963bbf70d0af3aa6a1e9e972afe4283
Asu <sdelang@sdelang.fr>
2021-06-17 01:20:22 +0200

    Lua build test stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,21 @@
 #include "pico/stdlib.h"
 #include <cstdlib>
 
+extern "C"
+{
+#include <lua.h>
+#include <lauxlib.h>
+}
+
 int main() {
     setup_default_uart();
     printf("Hello, world!\n");
+
+    lua_State* lua = luaL_newstate();
+
+    const char lua_script[] = "print('Hello, world (but cooler)!')";
+    int load_stat = luaL_loadbuffer(lua, lua_script, sizeof(lua_script) - 1, lua_script);
+    lua_pcall(lua, 0, 0, 0);
+
+    lua_close(lua);
 }
