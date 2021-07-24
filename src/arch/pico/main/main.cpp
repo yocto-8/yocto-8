a69bbfb2b9391d8c1aa741b67ef3814d935a33a2
Asu <sdelang@sdelang.fr>
2021-07-24 17:55:01 +0200

    Add commented out test program for compound ops and != syntax

--- src/arch/pico/main/main.cpp
+++ src/arch/pico/main/main.cpp
@@ -105,7 +105,7 @@ function _update()
 end
 __gfx__
 )";*/
-static constexpr std::string_view cartridge = R"(pico-8 cartridge // http://www.pico-8.com
+/*static constexpr std::string_view cartridge = R"(pico-8 cartridge // http://www.pico-8.com
 version 32
 __lua__
 r=64
@@ -121,6 +121,17 @@ function _update()
 		end
 	end
 end
+__gfx__
+)";*/
+static constexpr std::string_view cartridge = R"(pico-8 cartridge // http://www.pico-8.com
+version 32
+__lua__
+i=0
+i+=1
+if (i!=0) then print("uwu") end
+print(i)
+
+function _update() end
 __gfx__
 )";
 
