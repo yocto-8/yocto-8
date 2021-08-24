33b3fe2e4ebf50e918f6ee9989d7ed1d04fb6188
Asu <sdelang@sdelang.fr>
2021-08-24 21:43:14 +0200

    Clean up framebuffer logic on desktop, avoid using setPixel

--- src/arch/desktop/window.cpp
+++ src/arch/desktop/window.cpp
@@ -13,6 +13,7 @@ Window::Window() :
     window(sf::VideoMode(128, 128), "yocto-8")
 {
     window.setFramerateLimit(60);
+    fb_texture.create(128, 128);
 }
 
 void Window::present_frame(devices::Framebuffer fb)
@@ -30,29 +31,23 @@ void Window::present_frame(devices::Framebuffer fb)
 
     }
 
-    sf::Image image;
-    image.create(128, 128);
-    
-    for (std::size_t y = 0; y < fb.frame_height; ++y)
-    {
-        for (std::size_t x = 0; x < fb.frame_width; ++x)
-        {
-            const auto palette_entry = fb.get_pixel(x, y);
-            const auto color_rgb8 = video::pico8_palette_rgb8[palette_entry];
-            image.setPixel(x, y, sf::Color(
-                (color_rgb8 >> 16) & 0xFF,
-                (color_rgb8 >> 8) & 0xFF,
-                (color_rgb8 >> 0) & 0xFF
-            ));
-        }
-    }
+    std::array<std::uint8_t, fb.frame_width * fb.frame_height * 4> converted_fb;
 
-    fb_texture.loadFromImage(image);
+    for (std::size_t i = 0; i < fb.frame_bytes * 2; ++i)
+    {
+        const auto palette_entry = fb.get_nibble(i);
+        const auto color_rgb8 = video::pico8_palette_rgb8[palette_entry];
+        converted_fb[i * 4 + 0] = (color_rgb8 >> 16) & 0xFF;
+        converted_fb[i * 4 + 1] = (color_rgb8 >> 8) & 0xFF;
+        converted_fb[i * 4 + 2] = (color_rgb8 >> 0) & 0xFF;
+        converted_fb[i * 4 + 3] = 255;
+    }
 
     sf::View view = window.getView();
     view.setSize(128, 128);
     window.setView(view);
 
+    fb_texture.update(converted_fb.data());
     sf::Sprite sprite(fb_texture);
 
     window.draw(sprite);
