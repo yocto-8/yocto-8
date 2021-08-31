a4faf9cb31869ace9d5acd94cc360f09caf7dca5
Asu <sdelang@sdelang.fr>
2021-08-31 15:12:37 +0200

    Do not put the hardfault code into scratch because of stack overflows

--- src/arch/pico/extmem/faulthandler.hpp
+++ src/arch/pico/extmem/faulthandler.hpp
@@ -9,9 +9,9 @@ namespace arch::pico::extmem
 extern "C"
 {
 [[gnu::naked, gnu::used]] // owo
-void __scratch_y("isr_hardfault") isr_hardfault();
+void isr_hardfault();
 
 [[gnu::used]]
-void __scratch_y("hard_fault_handler_c") hard_fault_handler_c(std::uint32_t* args);
+void hard_fault_handler_c(std::uint32_t* args);
 }
 }
