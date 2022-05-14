b81cf54eda1a8df46baf8367f722ed4cf0f2ee3b
Asu <sdelang@sdelang.fr>
2022-05-14 23:37:32 +0200

    Handle rnd() with no parameters properly

--- src/emu/bindings/rng.cpp
+++ src/emu/bindings/rng.cpp
@@ -11,11 +11,18 @@ namespace emu::bindings
 int y8_rnd(lua_State* state)
 {
     // FIXME: this does not handle passing a table as a parameter yet
-    // FIXME: this does not handle rnd() (which would be rnd(1))
-    const auto max = luaL_checknumber(state, 1);
+
+    const auto argument_count = lua_gettop(state);
+
+    auto range = LuaFix16(1);
+
+    if (argument_count >= 1)
+    {
+        range = lua_tonumber(state, 1);
+    }
 
     // FIXME: this does not update random_state() in MMIO, nor does it follow the p8 algorithm.
-    lua_pushnumber(state, LuaFix16::from_fix16(rand() % max.value));
+    lua_pushnumber(state, LuaFix16::from_fix16(rand() % range.value));
 
     return 1;
 }
