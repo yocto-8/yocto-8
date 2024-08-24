4697b7465345ae55648ef504630f0eeb2c73a04e
asu <sdelang@sdelang.fr>
2024-08-24 16:07:04 +0200

    Whoops, fix test ROM path

--- src/tests/runtests.py
+++ src/tests/runtests.py
@@ -13,7 +13,7 @@ TEST_ROMS = list(str(path.relative_to(TEST_ROOT)) for path in TEST_ROOT.glob("**
 # thus you can filter tests for a specific file or directory with pytest's `-k` option, e.g. `-k rng/`
 @pytest.mark.parametrize("test_name", TEST_ROMS, ids=TEST_ROMS)
 def test_rom(test_name):
-    rom_path = TEST_ROOT / "roms" / test_name
+    rom_path = TEST_ROOT / test_name
     ground_truth = execute_test_rom_pico8(rom_path)
     exit_code, output = execute_test_rom_y8(rom_path)
     assert exit_code == 0, f"ROM `{test_name}` failed with code {exit_code}, output:\n{output}"
