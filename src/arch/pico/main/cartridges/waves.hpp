6d416fe6f0da32a36fd6a8586c5f6486cf5ae75d
asu <sdelang@sdelang.fr>
2024-09-07 14:47:28 +0200

    Fix dumb indent

--- src/arch/pico/main/cartridges/waves.hpp
+++ src/arch/pico/main/cartridges/waves.hpp
@@ -10,11 +10,11 @@ function _update60() end
 function _draw()
 	cls()
 	s=t()
-		for y=-r,r,3 do
-			for x=-r,r,2 do
-				local dist=sqrt(x*x+y*y)
-				z=cos(dist/40-s)*6
-				pset(r+x,r+y-z,7)
+	for y=-r,r,3 do
+		for x=-r,r,2 do
+			local dist=sqrt(x*x+y*y)
+			z=cos(dist/40-s)*6
+			pset(r+x,r+y-z,7)
 		end
 	end
 	printh(stat(1))
