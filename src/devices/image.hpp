4fadc9ccc8cde14c8375d7824309deb8e44f601b
asu <sdelang@sdelang.fr>
2024-10-03 17:56:12 +0200

    Move defs from image `.cpp` to `.hpp` for inlining
    We don't have LTO yet and those are small functions.

--- src/devices/image.hpp
+++ src/devices/image.hpp
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
 #include <emu/mmio.hpp>
 
 namespace devices {
@@ -14,14 +15,42 @@ struct Image : emu::MMIODevice<8192> {
 								 frame_bytes = (frame_width * frame_height) /
 	                                           frame_pixels_per_byte;
 
-	void clear(std::uint8_t palette_entry) const;
+	void clear(std::uint8_t palette_entry) const {
+		std::uint8_t pixel_pair_byte = palette_entry | (palette_entry << 4);
+		std::memset(data.data(), pixel_pair_byte, data.size());
+	}
 
-	void set_nibble(std::size_t i, std::uint8_t palette_entry) const;
+	void set_nibble(std::size_t i, std::uint8_t palette_entry) const {
+		std::uint8_t &pixel_pair_byte = data[i / 2];
+
+		if (i % 2 == 0) // lower pixel?
+		{
+			pixel_pair_byte &= 0xF0;                 // clear lower nibble
+			pixel_pair_byte |= palette_entry & 0x0F; // set lower nibble
+		} else {
+			pixel_pair_byte &= 0x0F;               // clear upper nibble
+			pixel_pair_byte |= palette_entry << 4; // set upper nibble
+		}
+	}
+
 	void set_pixel(std::uint8_t x, std::uint8_t y,
-	               std::uint8_t palette_entry) const;
+	               std::uint8_t palette_entry) const {
+		set_nibble(x + (y * frame_width), palette_entry);
+	}
 
-	std::uint8_t get_nibble(std::size_t i) const;
-	std::uint8_t get_pixel(std::uint8_t x, std::uint8_t y) const;
+	std::uint8_t get_nibble(std::size_t i) const {
+		std::uint8_t pixel_pair_byte = data[i / 2];
+
+		if (i % 2 == 0) // lower pixel?
+		{
+			return pixel_pair_byte & 0x0F;
+		}
+
+		return pixel_pair_byte >> 4;
+	}
+	std::uint8_t get_pixel(std::uint8_t x, std::uint8_t y) const {
+		return get_nibble(x + (y * frame_width));
+	}
 };
 
 struct Framebuffer : Image {
