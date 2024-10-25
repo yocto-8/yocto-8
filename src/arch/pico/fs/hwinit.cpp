02763c709d9cac379fa2e40a52e11e2d11238ef0
asu <sdelang@sdelang.fr>
2024-10-25 19:10:56 +0200

    Remove working buffer from mkfs

--- src/arch/pico/fs/hwinit.cpp
+++ src/arch/pico/fs/hwinit.cpp
@@ -14,10 +14,9 @@ void init_flash_fatfs() {
 		printf("Failed to mount flash FatFS (err=%d)\n", res);
 
 		// format flash partition
-		std::array<char, FF_MAX_SS> work_buffer;
 		MKFS_PARM fmt_opt = {FM_ANY, 0, 0, 0, 0};
-		res =
-			f_mkfs("/flash/", &fmt_opt, work_buffer.data(), work_buffer.size());
+		// FIXME: check if f_mkfs still works with no working buffer
+		res = f_mkfs("/flash/", &fmt_opt, nullptr, 0);
 
 		if (res != FR_OK) {
 			printf("Failed to format flash FatFS (err=%d)\n", res);
