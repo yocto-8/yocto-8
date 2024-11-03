5a9d26982f65f28a11bdf78e4605738960f7df42
asu <sdelang@sdelang.fr>
2024-11-03 12:01:48 +0100

    Fix linking in palette

--- src/video/palette.hpp
+++ src/video/palette.hpp
@@ -2,11 +2,10 @@
 
 #include <array>
 #include <cstdint>
-#include <span>
 
 namespace video {
 
-constexpr std::array<std::uint32_t, 32> pico8_palette_rgb8{
+static constexpr std::array<std::uint32_t, 32> pico8_palette_rgb8{
 	// 16 main colors
 	0x000000, 0x1D2B53, 0x7E2553, 0x008751, 0xAB5236, 0x5F574F, 0xC2C3C7,
 	0xFFF1E8, 0xFF004D, 0xFFA300, 0xFFEC27, 0x00E436, 0x29ADFF, 0x83769C,
@@ -22,7 +21,7 @@ constexpr std::uint32_t pack_rgb(std::uint8_t r, std::uint8_t g,
 	return (r << 16) | (g << 8) | b;
 }
 
-constexpr std::array<std::uint32_t, 32> ssd1351_precal_palette_rgb8{
+static constexpr std::array<std::uint32_t, 32> ssd1351_precal_palette_rgb8{
 	pack_rgb(0, 0, 0), pack_rgb(45, 60, 85), pack_rgb(100, 0, 80),
 	pack_rgb(0, 121, 54), pack_rgb(149, 70, 45), pack_rgb(100, 120, 100),
 	pack_rgb(150, 150, 180), pack_rgb(255, 230, 225), pack_rgb(255, 25, 90),
