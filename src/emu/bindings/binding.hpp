a696cd21f70ff47dcada5784736a7f5c844dc020
asu <sdelang@sdelang.fr>
2024-11-04 17:31:35 +0100

    Make some API functions be in SRAM

--- src/emu/bindings/binding.hpp
+++ src/emu/bindings/binding.hpp
@@ -2,3 +2,7 @@
 
 // Forward declare so we do not require including lua.h in every binding file
 typedef struct lua_State lua_State;
+
+/// Function attributes for functions part of the core API, which don't
+/// represent much code size, but should be consistently fast
+#define Y8_FAST_BINDING __attribute__((section(Y8_SRAM_SECTION), hot))
--- src/emu/bindings/table.hpp
+++ src/emu/bindings/table.hpp
@@ -4,9 +4,9 @@
 
 namespace emu::bindings {
 
-int y8_add(lua_State *state);
+Y8_FAST_BINDING int y8_add(lua_State *state);
 int y8_del(lua_State *state);
-int y8_foreach(lua_State *state);
+Y8_FAST_BINDING int y8_foreach(lua_State *state);
 int y8_split(lua_State *state);
 int y8_unpack(lua_State *state);
 
